1e80b2b45c63c1f4b657839101d97b904edb1a40
York Wang <yw544@nyu.edu>
2018-01-04 18:49:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
   # your code here
   array = [1,2,3]
-array.each {|a| return a*a}
+array.each do |number|
+  new_num = number*number
+  new_arrary.push(new_num)
+  new_arrary
+end
+return new_arrary
 end
